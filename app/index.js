8c525fedd606110c3dde97fcd4293db58efe782a
anugrahkresnaya <anugrahkresnaya@gmail.com>
2023-05-29 07:10:58 +0700

    refactor: add corsOptions

--- app/index.js
+++ app/index.js
@@ -6,8 +6,13 @@ const morgan = require('morgan')
 
 const app = express()
 
+const corsOptions = {
+  origin: true,
+  credentials: true
+}
+
 app.use(morgan(MORGAN_FORMAT))
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json())
 
 module.exports = router.apply(app)
