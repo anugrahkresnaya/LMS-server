9ba890efa25109ef27cd2b35c46c7546736d50f3
anugrahkresnaya <anugrahkresnaya@gmail.com>
2023-07-03 06:38:00 +0700

    refactor: add comment model and controller

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,10 +2,12 @@ const ApplicationController = require('./ApplicationController')
 const AuthenticationController = require('./AuthenticationController')
 const CourseController = require('./CourseController')
 const TransactionsController = require('./TransactionsController')
+const CommentController = require('./CommentController')
 
 module.exports = {
   ApplicationController,
   AuthenticationController,
   CourseController,
-  TransactionsController
+  TransactionsController,
+  CommentController
 }
